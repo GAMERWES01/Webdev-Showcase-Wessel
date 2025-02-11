dc7d4b6bb4b147c16c8ae63ac37cf639d7203967
GAMERWES01 <148088336+GAMERWES01@users.noreply.github.com>
2025-02-11 13:11:25 +0100

    api toegevoegt

---
+++ UC1-Profielpagina/Rest Api/Controllers/StudentsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Rest_Api.Controllers
+{
+    //[ApiController]
+    [Route("[controller]")]
+    public class StudentsController : ControllerBase
+    {
+
+        private static List<string> _students = new List<string>{
+            "Koen",
+            "Thijs",
+            "Rosalie"
+        };
+
+
+
+        //private readonly ILogger<StudentsController> _logger;
+
+        //public StudentsController(ILogger<StudentsController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        [HttpGet(Name = "GetStudents")]
+        public List<string> Get()
+        {
+            return _students;
+        }
+        [HttpPost(Name = "GetStudents")]
+        public void Post(string name)
+        {
+            if (_students.Contains(name))
+            {
+                _students.Add(name);
+            } 
+            
+        }
+    }
+}
